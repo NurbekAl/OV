56d41797a38d8752c3ffad013bb1095c882ea0e3
NurbekAl <nuriscom@mail.ru>
2017-04-20 21:11:16 +0600

    Change design and set textChangedListener

--- app/src/main/java/kg/obval/ov/Tablo.java
+++ app/src/main/java/kg/obval/ov/Tablo.java
@@ -10,22 +10,37 @@ import android.widget.TextView;
 
 public class Tablo extends AppCompatActivity {
 
-    EditText amountPresentEdit;
-    Spinner spinnerEst;
-    EditText amountNadoEdit;
-    Spinner spinnerNado;
+    TextView PokupkaView;
+    TextView ProdajaView;
+
     TextView dollarView;
     TextView dollarPokupkaView;
     TextView dollarProdazhaView;
+
     TextView euroView;
     TextView euroPokupkaView;
     TextView euroProdazhaView;
+
     TextView rublView;
     TextView rublPokupkaView;
     TextView rublProdazhaView;
+
     TextView tengeView;
     TextView tengePokupkaView;
     TextView tengeProdazhaView;
+
+    EditText kolsummaEdit;
+    Spinner  spinnervalutado;
+    TextView naView;
+    Spinner  spinnervalutaposle;
+    Spinner  kupitprodat;
+    TextView resultatView;
+    TextView summaView;
+    Spinner  proobmen;
+
+    double inputValue;
+
+    Calculator calculator;
 
 
 
@@ -35,8 +50,12 @@ public class Tablo extends AppCompatActivity {
         setContentView(R.layout.activity_tablo);
 
         init();
+        calculator = new Calculator;
 
-        amountPresentEdit.addTextChangedListener(new TextWatcher() {
+        setTextOnMoneyTextView();
+
+
+        kolsummaEdit.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -44,61 +63,51 @@ public class Tablo extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                inputValue = Double.parseDouble(kolsummaEdit.getText().toString());
+                summaView.setText(calculator.convert(inputValue, int usingCurrency,
+                                  int transferCurrency).toString());
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                calculator.setPresentAmount(Float.parseFloat(s.toString()));
-                amountNadoEdit.setText(String.valueOf(calculator.getNeededAmount()));
             }
         });
-
-        amountNadoEdit.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                calculator.setPresentAmount(Float.parseFloat(s.toString()));
-                amountPresentEdit.setText(String.valueOf(calculator.getPresentAmount()));
-
-
-
-
-
-
-
-
 
 
 
     }
 
     private void init() {
-        amountPresentEdit = (EditText) findViewById(R.id.amountEstEditText);
-        spinnerEst = (Spinner) findViewById(R.id.spinnerEst);
-        amountNadoEdit = (EditText) findViewById(R.id.amountNadoEdit);
-        spinnerNado = (Spinner) findViewById(R.id.spinnerNado);
+
+        PokupkaView = (TextView) findViewById(R.id.pokupkaView);
+        ProdajaView = (TextView) findViewById(R.id.prodajaView);
+
         dollarView = (TextView)  findViewById(R.id.dollarView);
         dollarPokupkaView = (TextView) findViewById(R.id.dollarPokupkaView);
         dollarProdazhaView = (TextView) findViewById(R.id.dollarProdazhaView);
+
         euroView = (TextView) findViewById(R.id.euroView);
         euroPokupkaView = (TextView) findViewById(R.id.euroPokupkaView);
         euroProdazhaView = (TextView) findViewById(R.id.euroProdazhaView);
+
         rublView = (TextView) findViewById(R.id.rublView);
         rublPokupkaView = (TextView) findViewById(R.id.rublPokupkaView);
-        rublProdazhaView = (TextView) findViewById(R.id.rublPokupkaView);
+        rublProdazhaView = (TextView) findViewById(R.id.rublProdajaView);
+
         tengeView = (TextView) findViewById(R.id.tengeView);
         tengePokupkaView = (TextView) findViewById(R.id.tengePokupkaView);
         tengeProdazhaView =(TextView) findViewById(R.id.tengeProdazhaView);
+
+        kolsummaEdit = (EditText)  findViewById(R.id.kolsummaedit);
+        spinnervalutado = (Spinner) findViewById(R.id.spinnervalutado);
+        naView = (TextView) findViewById(R.id.naView);
+        spinnervalutaposle = (Spinner) findViewById(R.id.spinnervalutaposle);
+        kupitprodat = (Spinner) findViewById(R.id.kupitprodat);
+        resultatView = (TextView) findViewById(R.id.resultatView);
+        summaView = (TextView) findViewById(R.id.summaView);
+        proobmen = (Spinner) findViewById(R.id.proobmen);
+
+
 
     }
 }
